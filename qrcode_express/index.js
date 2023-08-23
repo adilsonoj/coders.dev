8210300ad2abda64de61aeb6a440e46391052409
Adilson Oliveira <adilsonoj@yahoo.com.br>
2023-08-22 21:40:34 -0300

    add fluxo gerar qrcode

--- qrcode_express/index.js
+++ qrcode_express/index.js
@@ -3,21 +3,23 @@ import express from 'express';
 import { toDataURL } from 'qrcode';
 
 const app = express();
-const __dirname = "/home/arsenal/workspace/coders.dev/qrcode_express";
+const __dirname = "ABSOLUTE_PATH";
 
 const port = "8000"
-toDataURL('I am a pony!', function (err, url) {
-    console.log(url)
-})
-
-app.get('/', function (req, res) {
-    res.send('hello world');
-});
 
 app.get('/qrcode', function (req, res) {
+    const texto = req.query.texto
+
+    //gera o qrcode em data url
+    toDataURL(texto, function (err, url) {
+        res.send(url);
+    })
+});
+
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Serviço rodando na porta ${port}`)
 })
